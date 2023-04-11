71f8863feee7fc1991d02f7638819516e9e93ced
Rubia Mattos <rubia.matos@hotmail.com>
2023-04-11 20:16:08 -0300

    GrafoPonderado

--- codigo/GrafoPonderado.java
+++ codigo/GrafoPonderado.java
@@ -2,8 +2,37 @@ package codigo;
 
 public class GrafoPonderado extends GrafoMutavel {
 
-    public GrafoPonderado(String nome) {
-        super(nome);
+	public GrafoPonderado(String nome) {
+		super(nome);
+	}
+	
+	@Override
+    public boolean addAresta(int origem, int destino, int peso) {
+        boolean adicionou = false;
+        Vertice saida = this.existeVertice(origem);
+        Vertice entrada = this.existeVertice(destino);
+        if(saida!=null && entrada !=null){
+            saida.addAresta(destino,peso);
+            entrada.addAresta(origem,peso);
+            adicionou = true;
+        }
+        return adicionou;
     }
-
+	@Override
+	public Aresta removeAresta(int origem, int destino) {
+	    Vertice vOrigem = existeVertice(origem);
+	    Vertice vDestino = existeVertice(destino);
+	    
+	    if (vOrigem != null && vDestino != null) {
+	        Aresta aresta = vOrigem.existeAresta(destino);
+	        if (aresta != null) {
+	            vOrigem.removeAresta(destino);
+	            vDestino.removeAresta(origem);
+	            return new Aresta(destino);
+	        }
+	    }
+	    
+	    return null;
+	}
+    
 }
